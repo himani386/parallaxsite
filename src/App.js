b3c0c04d398b067b0fc5bad5a0f349b55b958390
himani386 <97656486+himani386@users.noreply.github.com>
2024-02-27 21:51:12 +0530

    parallaxsite

--- src/App.js
+++ src/App.js
@@ -3,22 +3,77 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+ 
+    <div class="parallax">
+      <div class="style2 team">
+      <h1 > RITIKA KUMARI</h1>
+      </div>
+      
+      <div class="style">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Thankyou for showing interest, 
+            here is a quick story of me and
+            this website. My programming 
+            journey started back in 2021, 
+            I started studying CSE in kec katihar. 
+            I am currently in my fourth 
+            year I like 
+            coding all kind of problems 
+            from the very basic like adding 
+            numbers to the complex ones like
+            competitive programming problems.
+            I love making projects and games.
+            Infact this website is one of my 
+            Web Development Projects which 
+            is built using HTML and CSS.<br></br>
+            Languages known : C Language, 
+            C++, JAVA, Python, JavaScript, 
+            MySQL.<br></br> IT Constructs : Data 
+            Structures and Algorithm, OOP, 
+            HTML, CSS, LINUX, and Git & GitHub.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div class="style2">
+        <h1>PROJECTS</h1>
+      </div>
+      <div class="style">
+        
+        <p>
+              Calendar ☀  
+            Tic-Tac-Toe ☀  
+            Quiz Game ☀  
+            Survey Form ☀  
+            Chat Bot in C ☀  
+            Tribute Webpage ☀  
+            Portfolio Website ☀  
+            Guess the Number ☀  
+            Rock Paper Scissor ☀ 
+            To-Do List using JS ☀  
+            Notes Taker using JS ☀  
+            BMI Calculator using JS ☀ 
+            Loan Calculator using JS ☀  
+            Travel Management System ☀  
+            Random Password Generator ☀  
+            Different Management Systems
+        </p>
+        
+      </div>
+      <div class="style2">
+        <h1>ACHIEVEMENTS</h1>
+      </div>
+      <div class="style3">
+      <p >
+              
+            Microsoft Learn Student 
+            Ambassador - Beta ☀  
+            Participant in GirlScript Summer 
+            of Code 2021 ☀  
+            Mentored HackTX Hackathon
+        </p>
+      </div>
     </div>
+    
+
   );
 }
 
